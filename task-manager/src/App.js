96d8850821929e9669099af26c244661c20ceeba
Vaishnavi T V <140780960+TVVaishnavi@users.noreply.github.com>
2025-12-08 10:26:47 +0530

    Remove BrowserRouter and use Fragment instead

--- task-manager/src/App.js
+++ task-manager/src/App.js
@@ -3,11 +3,11 @@ import Header from "./component/Header";
 import Home from "./pages/Home";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <ToastContainer position="top-right" autoClose={1500} />
 
       <Header />
@@ -15,8 +15,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
 
 export default App;
+
