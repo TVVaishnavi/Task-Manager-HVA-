a6e2fa3a929ce8dd167f3ab50c6c8d154980159a
Vaishnavi T V <140780960+TVVaishnavi@users.noreply.github.com>
2025-12-08 10:18:37 +0530

    Remove Routers component and related imports

--- task-manager/src/component/Routers.js
+++ task-manager/src/component/Routers.js
@@ -1,15 +1,1 @@
-import React from 'react';
-import {Route, Routes} from 'react-router-dom';
-import Home from '../pages/Home';
 
-function Routers(){
-    return(
-        <div>
-            <Routes>
-                <Route path='/home' element={<Home />} />
-            </Routes>
-        </div>
-    )
-}
-
-export default Routers;
