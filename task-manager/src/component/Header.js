845aecd4b3021a2c5fea68583d49b84b4b9bc974
Vaishnavi T V <140780960+TVVaishnavi@users.noreply.github.com>
2026-01-07 14:14:34 +0530

    Remove filter button from Header component
    Removed filter button from the header component.

--- task-manager/src/component/Header.js
+++ task-manager/src/component/Header.js
@@ -1,11 +1,9 @@
 import React, {useState} from 'react';
 import '../style/Header.css';
-import { HiFilter } from "react-icons/hi";
 
 function Header({ openForm, toggleFilter, searchBar, setSearchBar }){
     return(
         <div className='header'>
-            <button className='icon' onClick={toggleFilter}><HiFilter  size={28}/></button>
             <h1>Task Manager</h1>
 
             <input type='text' className='search' placeholder='search task with title' value={searchBar} 
